188ecb2452559ad7f672db2e159c272ab8d99cb4
caodongzhen <caodongzhen@ybm100.com>
2019-06-10 15:55:45 +0800

    some change

--- service-fegin/src/main/java/com/example/demo/controller/HiController.java
+++ service-fegin/src/main/java/com/example/demo/controller/HiController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +15,12 @@ public class HiController {
 	@Autowired
 	private SchedualServiceHi schedualServiceHi;
 
+	@Value("${server.port}")
+	String port;
+
 	@RequestMapping(value = "/hi", method = RequestMethod.GET)
 	public String hi(@RequestParam String name) {
-		return schedualServiceHi.sayHiFromClientOne(name);
+		return "hi " + name + ",i am from port:" + port + " " + schedualServiceHi.sayHiFromClientOne(name);
 	}
 
 }
--- service-ribbon/src/main/java/com/example/demo/controller/HelloController.java
+++ service-ribbon/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,12 @@ public class HelloController {
 	@Autowired
 	private HelloService helloService;
 
+	@Value("${server.port}")
+	String port;
+
 	@RequestMapping("/hi")
 	public String hi(@RequestParam String name) {
-		return helloService.hiService(name);
+		return "hi " + name + ",i am from port:" + port + " " + helloService.hiService(name);
 	}
 
 }
